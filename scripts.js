b6745403d559bd053895763a0b4591176cd82fdb
Wecode <mashleyalpha@gmail.com>
2018-12-21 11:06:35 +0200

    pending changes in java scripts

--- scripts.js
+++ scripts.js
@@ -10,66 +10,62 @@ function check() {
   var questionNine = document.quiz.questionNine.value;
   var questionTen = document.quiz.questionTen.value;
   var questionEleven = document.quiz.questionEleven.value;
+  var questionTwelve = document.quiz.questionTwelve.value;
+  var questionThirteen = document.quiz.questionThirteen.value;
+  var questionFourteen = document.quiz.questionFourteen.value;
+  var questionFifteen = document.quiz.questionFifteen.value;
   var count = 0;
 
   if (questionOne == "2") {
-    count += 5;
+    count += 4;
   }
   if (questionTwo == "2") {
-    count += 5;
+    count += 4;
   }
   if (questionThree == "3") {
-    count += 5;
+    count += 4;
   }
   if (questionFour == "3") {
-    count += 5;
+    count += 4;
   }
   if (questionFive == "4") {
-    count += 5;
+    count += 4;
   }
   if (questionSix == "2") {
-    count += 5;
+    count += 4;
   }
   if (questionSeven == "2") {
-    count += 5;
+    count += 4;
   }
   if (questionEight == "3") {
-    count += 5;
+    count += 4;
   }
   if (questionNine == "2") {
-    count += 5;
+    count += 4;
   }
   if (questionTen == "4") {
-    count += 5;
+    count += 4;
   }
-  $(".questionEleven").change(function() {
-    var questionElevenbool = true;
-
-    var questionElevenanswers = new Array();
-
-    var questionElevenright = new Array("kicukiro", "nyarugenge");
-
-    $(".questionEleven input:checked").each(function() {
-      questionElevenanswers.push($(this).val());
-    });
-
-    questionElevenanswers.sort();
-
-    questionElevenright.sort();
-
-    if (questionElevenanswers.length == questionElevenright.length) {
-      for (i = 0; i < questionElevenanswers.length; i++) {
-        if (questionElevenanswers[i] == questionElevenright[i]) {
-          count += 5;
-        }
-      }
-    }
-  });
+  if (questionEleven == "1") {
+    count += 4;
+  }
+  if (questionTwelve == "3") {
+    count += 4;
+  }
+  if (questionThirteen == "4") {
+    count += 4;
+  }
+  if (questionFourteen == "1") {
+    count += 4;
+  }
+  if (questionFifteen == "4") {
+    count += 4;
+  }
 
   document.getElementById("after_submit").style.visibility = "visible";
 
   document.getElementById("number_count").innerHTML =
-    "Your score is: " + count + "/50";
+    "Your score is: " + count + "/60";
 
   $("#button").hide();
   $("#quiz").hide();
